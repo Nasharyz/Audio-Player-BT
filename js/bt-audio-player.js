48527864a7d5e06874885449868657f808ab0b79
Nasharyz <floriane.jacquemin2@gmail.com>
2026-06-10 23:25:18 +0200

    Enhance audio player with volume control and track info
    Updated the HTML structure of the audio player to include volume control and track information.

--- js/bt-audio-player.js
+++ js/bt-audio-player.js
@@ -7,14 +7,50 @@ document.querySelectorAll(".bt-audioplayer").forEach(player => {
 console.log("AVANT innerHTML");
     player.innerHTML = `
     <audio class="bt-audio"></audio>
+<div class="bt-track-info">
 
-    <div class="bt-track-info">
-<div class="bt-track-text">
+        <div class="bt-time-row">
+
+            <div class="bt-play-button">
+                <span class="bt-icon"></span>
+            </div>
+
+            <div class="bt-progress-bar">
+                <div class="bt-progress-fill"></div>
+            </div>
+            <span class="bt-time-display">
+                <span class="bt-current-time">0:00</span>
+                /
+                <span class="bt-duration">0:00</span>
+            </span>
+            <div class="bt-volume-control">
+                <span class="bt-volume-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" style="color: currentColor;" width="64" height="64" viewBox="0 0 24 24">
+                        <g fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.08 9H5a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h.08a2 2 0 0 1 1.519.698l3.642 4.25c.604.704 1.759.277 1.759-.651V4.703c0-.928-1.155-1.355-1.76-.65L6.6 8.301A2 2 0 0 1 5.08 9zm13.556-4.725a1 1 0 1 0-1.377 1.45c3.655 3.472 3.655 9.078 0 12.55a1 1 0 1 0 1.377 1.45c4.485-4.26 4.485-11.19 0-15.45zm-2.947 2.8a1 1 0 1 0-1.378 1.45c2.027 1.925 2.027 5.025 0 6.95a1 1 0 1 0 1.378 1.45c2.857-2.714 2.857-7.136 0-9.85z"
+                        fill="currentColor">
+                            </path>
+                        </g>
+                    </svg>
+
+                </span>
+
+                <input
+                    type="range"
+                    class="bt-volume-slider"
+                    min="0"
+                    max="1"
+                    step="0.01"
+                    value="0.75">
+            </div>
+        </div>
+        <div class="bt-track-text">
             <span class="bt-title"></span>
             <span class="bt-separator"> — </span>
             <span class="bt-artist"></span>
         </div>
     </div>
+</div>
 `;
     console.log("APRÈS innerHTML");
     const audio = player.querySelector(".bt-audio");
