70f6fd79e1f4ca474d7c252ba39566e13a083817
Nasharyz <floriane.jacquemin2@gmail.com>
2026-06-11 21:17:47 +0200

    Update bt-audio-player.js

--- js/bt-audio-player.js
+++ js/bt-audio-player.js
@@ -85,7 +85,7 @@ artist.textContent =
 const playSVG = `
 <svg viewBox="0 0 24 24" width="100%" height="100%">
         <path
-            transform="translate(3,0)"
+            transform="translate(8,0)"
             fill="currentColor"
             d="M6 5.912c0-.155.037-.307.107-.443c.23-.44.75-.599 1.163-.354l10.29 6.088c.14.083.255.206.332.355c.23.44.08.995-.332 1.239L7.27 18.885a.8.8 0 0 1-.415.115C6.383 19 6 18.592 6 18.089z"/>
     </svg>`;
