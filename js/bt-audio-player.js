a5d279af353a84351fa8a5431ca709e86b3a3bec
Nasharyz <floriane.jacquemin2@gmail.com>
2026-06-12 00:23:43 +0200

    Modify playSVG path in bt-audio-player.js
    Updated playSVG to use a new SVG path for play icon.

--- js/bt-audio-player.js
+++ js/bt-audio-player.js
@@ -84,11 +84,10 @@ artist.textContent =
 
 const playSVG = `
 <svg viewBox="0 0 24 24" width="100%" height="100%">
-        <path
-            fill="currentColor"
-            fill-rule="evenodd"
-            d="M133 440a35.37 35.37 0 0 1-17.5-4.67c-12-6.8-19.46-20-19.46-34.33V111c0-14.37 7.46-27.53 19.46-34.33a35.13 35.13 0 0 1 35.77.45l247.85 148.36a36 36 0 0 1 0 61l-247.89 148.4A35.5 35.5 0 0 1 133 440"/>
-    </svg>`;
+    <path fill="currentColor"
+        fill-rule="evenodd"
+        d="M10 18a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1zm7 0a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1z"/>
+</svg>`;
 
 const pauseSVG = `
 <svg viewBox="0 0 24 24" width="100%" height="100%">
